0118908f9174fa2ad7fe325f453bb1efdb8762d4
Polothevets Oleg <olegpolothevets@gmail.com>
2023-11-19 16:33:17 +0200

    add function delete user

--- admin-panel/src/components/users/Users.jsx
+++ admin-panel/src/components/users/Users.jsx
@@ -1,10 +1,8 @@
-// Users.jsx
 import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import k from './users.module.scss';
 
 const Users = () => {
-    // Приклад даних про користувачів (використовуйте свої дані або отримайте їх з сервера)
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
@@ -13,6 +11,20 @@ const Users = () => {
             .then(data => setUsers(data))
             .catch(error => console.error('Error fetching users:', error));
     }, []);
+
+    const handleDeleteUser = async (userId) => {
+        // Виконайте HTTP DELETE-запит для видалення користувача з бази даних
+        const response = await fetch(`http://localhost:3001/users/${userId}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            // Видаліть користувача зі стану після успішного видалення
+            setUsers(prevUsers => prevUsers.filter(user => user.id !== userId));
+        } else {
+            console.error('Failed to delete user');
+        }
+    };
 
     return (
         <div className={k.wrapper}>
@@ -24,7 +36,7 @@ const Users = () => {
                         <div>{user.password}</div>
                         <div>{user.email}</div>
                         <Link to={`/users/${user.id}`}>View Profile</Link>
-                        <button>X</button>
+                        <button onClick={() => handleDeleteUser(user.id)}>X</button>
                     </li>
                 ))}
             </ul>
